b4e9937cc8ce3bf8b272300033cf03c13c4dbff0
Ivi_ <103999115+VelyIvi@users.noreply.github.com>
2023-10-11 10:21:51 +0200

    Added more decorations and merging into main

--- main.js
+++ main.js
@@ -63,15 +63,50 @@ var icoSphere = new THREE.Mesh(
     })
 );
 
-
 scene.add(icoSphere);
+
+
+var torusKnotGreen = new THREE.Mesh(
+    new THREE.TorusKnotGeometry( 0.4, 0.001,  64, 128, 3, 2),
+    new THREE.MeshBasicMaterial( { color: 0x6E0D25})
+);
+
+
+var torusKnotBlue = new THREE.Mesh(
+    new THREE.TorusKnotGeometry( 0.5, 0.001,  64, 128, 3, 5),
+    new THREE.MeshBasicMaterial( { color: 0x9E768F})
+);
+
+scene.add(torusKnotGreen);
+scene.add(torusKnotBlue);
+
+var decorationCube1 = new THREE.Mesh(
+    new THREE.BoxGeometry( 0.3,0.3, 0.3),
+    new THREE.MeshBasicMaterial( { color: 0xffffff, wireframe: true})
+);
+decorationCube1.position.z = -0.5;
+scene.add(decorationCube1);
+
+var decorationCube2 = new THREE.Mesh(
+    new THREE.BoxGeometry( 0.2,0.2, 0.2),
+    new THREE.MeshBasicMaterial( { color: 0xffffff, wireframe: true})
+);
+decorationCube2.position.z = -0.1;
+scene.add(decorationCube2);
+
+var decorationCube3 = new THREE.Mesh(
+    new THREE.BoxGeometry( 0.15,0.15, 0.15),
+    new THREE.MeshBasicMaterial( { color: 0xffffff, wireframe: true})
+);
+decorationCube3.position.z = -0.3;
+scene.add(decorationCube3);
+
 
 // icoSphere.position.x =0.7;
 // icoSphere.position.y =0.4;
 
 
 
-var clock = new THREE.Clock();
 
 
 var deltaMouse = (0.0, 0.0);
@@ -91,10 +126,11 @@ function scrollAnimate(){
 
     const t = -document.body.getBoundingClientRect().top/(height-windowInnerHeight);
     const tPow = Math.pow(t, 2);
-    console.log(t, "  ", height, "   ", window.innerHeight);
+    // console.log(t, "  ", height, "   ", window.innerHeight);
     icoSphere.material.uniforms.uChange.value = t*0.9;
 
     camera.position.set(-0.7 + t*1.4, -0.4 + t*0.8, 1.5);
+
 }
 
 
@@ -108,10 +144,17 @@ function onWindowResize(){
 }
 
 window.addEventListener( 'resize', onWindowResize, false );
+
+
+
+var clock = new THREE.Clock();
+var delta = 0;
+
 function animate() {
     document.body.onscroll = scrollAnimate();
 
     var elapsedTime = clock.getElapsedTime();
+    delta = clock.getDelta();
 
 
     icoSphere.material.uniforms.uTime.value = 0.5*elapsedTime;
@@ -122,9 +165,38 @@ function animate() {
 
 
     requestAnimationFrame( animate );
-    // icoSphere.rotation.x += 0.002;
-    // icoSphere.rotation.y += 0.0005;
-    // icoSphere.rotation.z += 0.002;
+    torusKnotGreen.rotation.x += 0.0005;
+    torusKnotGreen.rotation.y += 0.0005;
+    torusKnotGreen.rotation.z += 0.0005;
+
+    torusKnotBlue.rotation.x -= 0.0005;
+    torusKnotBlue.rotation.y += 0.0005;
+    torusKnotBlue.rotation.z -= 0.0008;
+
+
+    decorationCube1.rotation.x += 0.001;
+    decorationCube1.rotation.y += 0.001;
+    decorationCube1.rotation.z += 0.001;
+
+    decorationCube2.rotation.x -= 0.002;
+    decorationCube2.rotation.y += 0.0015;
+    decorationCube2.rotation.z -= 0.001;
+
+    decorationCube3.rotation.x += 0.003;
+    decorationCube3.rotation.y += 0.0005;
+    decorationCube3.rotation.z -= 0.001;
+
+    decorationCube1.position.y = Math.sin(elapsedTime/2)/10+0.9;
+    decorationCube1.position.x = Math.sin(elapsedTime/20)/8+1.5;
+
+    decorationCube2.position.y = Math.cos(elapsedTime*1.3)/20+0.3;
+    decorationCube2.position.x = Math.sin(elapsedTime/10)/10+1.2;
+
+    decorationCube3.position.y = Math.sin(elapsedTime*1.8+26)/25+0.5;
+    decorationCube3.position.x = Math.sin(elapsedTime/30)/10+1.8;
+
+
+    // console.log(sinTest);
 
     renderer.render( scene, camera );
 
